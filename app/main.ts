1cb028e0cc85367217e6a83bdcdac26c54a18c1a
CJ Avilla <cjavilla@gmail.com>
2016-02-18 08:12:00 -0800

    ignore logs

--- app/main.ts
+++ app/main.ts
@@ -4,16 +4,24 @@ import { bootstrap } from 'angular2/bootstrap';
 import { Component } from 'angular2/core';
 import * as Rx from 'rx';
 import { FirebaseService } from 'angular2-firebase/core';
-import { Topic } from './topics';
+import { Topic, TopicsComponent } from './topics';
 import {provide} from 'angular2/core';
+
 declare var Firebase: any;
 
 @Component({
   selector: 'app',
-  template: '<h1>Test</h1>'
+  template: '<h1>Test<topics [topics]="topics"></topics></h1>',
+  directives: [TopicsComponent]
 })
 class App {
-    
+  topics: Topic[];
+  
+  constructor() {
+    this.topics = [{
+      title: 'test'
+    }];
+  }
 }
 var fb = new Firebase('https://reno-topics.firebaseio.com');
 bootstrap(App);
--- app/topics.ts
+++ app/topics.ts
@@ -1,7 +1,28 @@
+'use strict';
+
+import { Component, Input } from 'angular2/core';
+
 export interface ITopic {
   title: String;
 }
 
-export class Topic extends ITopic {
+export class Topic {
   title: String;
+}
+
+@Component({
+  selector: 'topic',
+  template: '<li>{{ topic | json }}</li>'
+})
+export class TopicComponent { 
+  @Input() topic: Topic;
+}
+
+@Component({
+  selector: 'topics',
+  template: '<ul>{{topics.length}}<topic [topic]="topic" ngFor="#topic of topics"></topic></ul>',
+  directives: [TopicComponent]
+})
+export class TopicsComponent {
+  @Input() topics: Topic[];
 }
